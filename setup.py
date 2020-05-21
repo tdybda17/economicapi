c80134a6a61d4ad65d014e09467b8466843b6a53
Tobias Dybdahl <tdybda17@student.aau.dk>
2020-05-21 15:58:58 +0200

    added setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name='economicapi',
-    version='0.1',
-    scripts=['api'],
-    author="Tobias Dybdahl",
-    author_email="tobiasdybdahl@gmail.com",
-    description="A api for creating e-conomic orders",
-    long_description="Some long description",
+    name="economicapi-tdybda", # Replace with your own username
+    version="0.0.1",
+    author="tdybda17",
+    author_email="tdybda17@student.aau.dk",
+    description="E-conomic order api",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tdybda17/economicapi",
     packages=setuptools.find_packages(),
@@ -16,4 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.5',
 )
