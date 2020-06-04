73adcec81ebd0de49844950fd104b86f406b7292
Tobias Dybdahl <tdybda17@student.aau.dk>
2020-06-04 12:38:51 +0200

    rest api integrated

--- economic_dybdahl_rest/urls.py
+++ economic_dybdahl_rest/urls.py
@@ -5,5 +5,5 @@ from economic_dybdahl_rest.endpoints.sync_product import SyncProductEndpoint
 
 urlpatterns = [
     # path('v1/order/', OrderEndpoint.as_view(), name='order endpoint'),
-    path('v1/sync-product', SyncProductEndpoint.as_view(), name='sync product endpoint'),
+    path('v1/product/', SyncProductEndpoint.as_view(), name='sync product endpoint'),
 ]
