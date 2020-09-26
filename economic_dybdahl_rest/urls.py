0f38484b36f9fe6e9d8b15950498ff266a491d0f
tobiasdybdahl <tdybda17@student.aau.dk>
2020-09-26 13:41:37 +0200

    fixed get product

--- economic_dybdahl_rest/urls.py
+++ economic_dybdahl_rest/urls.py
@@ -4,5 +4,5 @@ from economic_dybdahl_rest.endpoints.product import ProductEndpoint
 
 urlpatterns = [
     path('v1/product/', ProductEndpoint.as_view(), name='sync product endpoint'),
-    path('v1/product/<int:product_number>/', ProductEndpoint.as_view(), name='product endpoint')
+    path('v1/product/<str:product_number>/', ProductEndpoint.as_view(), name='product endpoint')
 ]
