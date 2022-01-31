40110789a16e244d681240f617ac5f99827dff6f
dtidemann <dannytidemann@gmail.com>
2022-01-31 18:32:01 +0100

    Fix

--- economicapi/settings.py
+++ economicapi/settings.py
@@ -12,7 +12,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -23,7 +22,6 @@ SECRET_KEY = '__SOMETHING__'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -76,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'economicapi.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -86,7 +83,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -106,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -120,16 +115,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-with open(os.path.join(BASE_DIR, 'credentials.json')) as json_file:
-    config = json.load(json_file)
+try:
+    with open(os.path.join(BASE_DIR, 'credentials.json')) as json_file:
+        config = json.load(json_file)
+except:
+    config = dict()
 
 X_APP_SECRET_TOKEN = config.get('X_APP_SECRET_TOKEN', 'demo')
 X_AGREEMENT_GRANT_TOKEN = config.get('X_AGREEMENT_GRANT_TOKEN', 'demo')
