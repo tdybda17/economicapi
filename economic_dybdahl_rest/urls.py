5582180014558da5f0dad253ec1be2c22cf65659
Malthe <mbe@sigmanet.dk>
2023-02-21 10:55:38 +0100

    Update urls.py

--- economic_dybdahl_rest/urls.py
+++ economic_dybdahl_rest/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('v1/product/', ProductEndpoint.as_view(), name='sync product endpoint'),
     path('v1/product/<str:product_number>/', ProductEndpoint.as_view(), name='product endpoint'),
     path('v1/product/<str:product_number>/block/', BlockProductEndpoint.as_view(), name='block product endpoint'),
-    path('v1/all_customers/', AllCustomersEndpoint.as_view(), name='get customer endpoint'),
+    path('v1/customers/', AllCustomersEndpoint.as_view(), name='get customer endpoint'),
     path('v1/customers/<str:customer_number>/', CustomersEndpoint.as_view(), name='get customer endpoint'),
     path('v1/supplier/invoice/', SupplierInvoiceWithLinesEndpoint.as_view(), name='supplier invoice with lines'),
     path('v1/supplier/invoice/<int:id>/', SupplierInvoiceWithLinesEndpoint.as_view(),
