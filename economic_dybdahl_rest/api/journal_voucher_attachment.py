d703c664af8c2ae6287d99125edfe84c6ca703c0
tobiasdybdahl <td@sigmanet.dk>
2024-07-16 12:49:39 +0200

    Overwritten headers

--- economic_dybdahl_rest/api/journal_voucher_attachment.py
+++ economic_dybdahl_rest/api/journal_voucher_attachment.py
@@ -6,12 +6,12 @@ from economic_dybdahl_rest.api._api import EconomicApi
 class JournalVoucherAttachmentAPI(EconomicApi):
 
     path = 'journals/'
+    updated_headers = dict()
 
     def __init__(self) -> None:
-        self.headers['Content-Type'] = None
+        self.updated_headers = dict(self.headers)
+        self.updated_headers['Content-Type'] = None
         super().__init__(self.path)
-
-
 
     def patch(self, journal_number, accounting_year_voucher_number, data):
         response = requests.patch(
